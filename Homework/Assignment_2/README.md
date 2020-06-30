77eedbafa793bbdce41644886cb7f888b0a9d582
SG2765769 <sgillam1@student.rccd.edu>
2020-06-29 21:32:50 -0700

    Signed-off-by: SG2765769 <sgillam1@student.rccd.edu>

--- Homework/Assignment_2/README.md
+++
@@ -1,2 +0,0 @@
-# GillamSean_CIS17A_46721
-Secondary C++ Class RCC Summer 2020
---
+++ Homework/IntroLab/Hello_World/main.cpp
@@ -0,0 +1,16 @@
+/* 
+ * File:   main.cpp
+ * Author: Sean Gillam
+ * Class: CIS-17A-46721
+ * Created on June 29, 2020, 9:22 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+int main() 
+{
+    cout << "Hello World";
+    return 0;
+}
+
